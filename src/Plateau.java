a1099006167ab93edaff71185cd01df51a19d443
MAM <ma.majeau@gmail.com>
2015-11-03 15:28:11 -0500

    Ajout dune petite fonction pour voir tous case qui sont occuper sur le plateau

--- src/Plateau.java
+++ src/Plateau.java
@@ -1,3 +1,4 @@
+import java.util.Enumeration;
 import java.util.Hashtable;
 
 /**
@@ -13,5 +14,26 @@ public class Plateau {
         this.listeCase = listeCase;
         this.casesOccupees = casesOccupees;
         this.board = board;
+
+        listeCase();
+    }
+
+    //Petite fonction qui permet de passer a travers la liste de pion et qui donne leur case et le nombre de case
+    public void listeCase()
+    {
+        Hashtable liste=casesOccupees;
+        Enumeration items = liste.keys();
+        int nbPion=0;
+
+        while(items.hasMoreElements())
+        {
+            String strKey = (String)items.nextElement();
+            Case caseSimple=(Case)liste.get(strKey);
+            String strValue=caseSimple.occupant.getClass().toString();
+            nbPion+=1;
+
+            System.out.println(strKey+" "+strValue);
+        }
+        System.out.println("Nombre de pion sur le tableau="+nbPion);
     }
 }
